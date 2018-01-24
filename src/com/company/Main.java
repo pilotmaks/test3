cc6b6457d1410ebb8fb84ba898588cfbb2eb12f6
Pilot Maks <pilotmaks@mail.ru>
2018-01-24 06:50:25 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String word = "CA";
+        char guess1;
+        boolean guessedFirstLetter = false;
+        System.out.print("Hi, Maks! Input: ");
+        Scanner keyboard = new Scanner(System.in);
+        int myStr = keyboard.nextInt();
+        System.out.print(myStr);
+    }
+}
